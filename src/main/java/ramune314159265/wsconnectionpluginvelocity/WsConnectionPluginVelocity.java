55befc69594157ca0a700d2818148d1a9e5f62d8
ramune314159265 <ramune314159265@gmail.com>
2024-02-25 13:51:43 +0900

    サーバー起動イベントを送るように

--- src/main/java/ramune314159265/wsconnectionpluginvelocity/WsConnectionPluginVelocity.java
+++ src/main/java/ramune314159265/wsconnectionpluginvelocity/WsConnectionPluginVelocity.java
@@ -2,11 +2,13 @@ package ramune314159265.wsconnectionpluginvelocity;
 
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ListenerBoundEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
+import ramune314159265.wsconnectionpluginvelocity.events.ServerStartedEvent;
 import ramune314159265.wsconnectionpluginvelocity.events.ServerStoppedEvent;
 
 @Plugin(
@@ -37,6 +39,11 @@ public class WsConnectionPluginVelocity {
 
 		server.getEventManager().register(this, new PluginListener());
 	}
+
+    @Subscribe
+    public void onListenerBounded(ListenerBoundEvent event){
+        WsConnectionPluginVelocity.wsConnection.sendEventData(new ServerStartedEvent());
+    }
 
 	@Subscribe
 	public void onProxyShutdown(ProxyShutdownEvent event) {
