eb1fce8d855bb8940acb4fbbd9f935602a3282b7
ramune314159265 <ramune314159265@gmail.com>
2024-03-15 14:38:21 +0900

    connectしていないプレイヤーはdisconnectを送信しないように

--- src/main/java/ramune314159265/wsconnectionpluginvelocity/PluginListener.java
+++ src/main/java/ramune314159265/wsconnectionpluginvelocity/PluginListener.java
@@ -3,24 +3,22 @@ package ramune314159265.wsconnectionpluginvelocity;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.DisconnectEvent;
 import com.velocitypowered.api.event.player.PlayerChatEvent;
-import com.velocitypowered.api.event.player.ServerPreConnectEvent;
+import com.velocitypowered.api.event.player.ServerConnectedEvent;
 import ramune314159265.wsconnectionpluginvelocity.events.*;
-
-import java.util.Objects;
 
 public class PluginListener {
 	@Subscribe()
-	public void onServerPostConnectEvent(ServerPreConnectEvent event) {
-		String joinedServerId = event.getOriginalServer().getServerInfo().getName();
+	public void onServerConnectedEvent(ServerConnectedEvent event) {
+		String joinedServerId = event.getServer().getServerInfo().getName();
 		String playerId = event.getPlayer().getUsername();
 
 		WsConnectionPluginVelocity.playerConnectingServerMap.put(playerId, joinedServerId);
 
 		Event eventDataToSend;
-		if (Objects.isNull(event.getPreviousServer())) {
+		if (event.getPreviousServer().isEmpty()) {
 			eventDataToSend = new PlayerConnectedEvent(joinedServerId, playerId);
 		} else {
-			String previousJoinedServerId = event.getPreviousServer().getServerInfo().getName();
+			String previousJoinedServerId = event.getPreviousServer().get().getServerInfo().getName();
 			eventDataToSend = new PlayerMovedEvent(joinedServerId, previousJoinedServerId, playerId);
 		}
 
@@ -30,15 +28,19 @@ public class PluginListener {
 	@Subscribe
 	public void onDisconnectEvent(DisconnectEvent event) {
 		String playerId = event.getPlayer().getUsername();
+		if(WsConnectionPluginVelocity.playerConnectingServerMap.get(playerId).isEmpty()){
+			return;
+		}
 		String previousJoinedServerId = WsConnectionPluginVelocity.playerConnectingServerMap.get(playerId);
 		WsConnectionPluginVelocity.wsConnection.sendEventData(new PlayerDisconnectedEvent(playerId, previousJoinedServerId));
+		WsConnectionPluginVelocity.playerConnectingServerMap.remove(playerId);
 	}
 
 	@Subscribe
 	public void onPlayerChattedEvent(PlayerChatEvent event){
 		String playerId = event.getPlayer().getUsername();
 		String content = event.getMessage();
-		String serverId = event.getPlayer().getUsername();
+		String serverId = WsConnectionPluginVelocity.playerConnectingServerMap.get(playerId);
 		WsConnectionPluginVelocity.wsConnection.sendEventData(new PlayerChattedEvent(content,playerId,serverId));
 	}
 }
